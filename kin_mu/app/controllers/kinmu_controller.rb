c7af702caff3bc6025e37da725f7ce27dae5c61f
山崎孝洋 <tak_3_zaki3@yahoo.co.jp>
2014-09-30 08:25:48 +0900

    時刻問題解決

--- kin_mu/app/controllers/kinmu_controller.rb
+++ kin_mu/app/controllers/kinmu_controller.rb
@@ -8,7 +8,6 @@ class KinmuController < ApplicationController
     @today = Date.today
     @user_id = current_user.id
     @record_date = GetDspDate(params[:dsp_date])
-    puts @record_date
 
     begin
       db_serch = Roster.find_by!(record_date: @record_date, user_id: @user_id)
@@ -17,10 +16,11 @@ class KinmuController < ApplicationController
       @roster_db.end_working       = db_serch.end_working.strftime("%X")
       @roster_db.work_description  = db_serch.work_description
     rescue Exception => e
-      puts "--例外発生 #{e}\n"
-      @roster_db.working_start    = DateTime.now.strftime("%X")
-      @roster_db.end_working      = DateTime.now.strftime("%X")
+      @roster_db.working_start    = @record_date.to_time.to_datetime
+      @roster_db.end_working      = @record_date.to_time.to_datetime
       @roster_db.work_description = ""
+      puts "--例外発生 #{e} #{@record_date}　#{@roster_db.end_working.class}"
+
     end
 
   end
@@ -28,7 +28,7 @@ class KinmuController < ApplicationController
   def show
     @date_from = Date.today.beginning_of_month
     @date_to   = Date.today.end_of_month
-    @serch_result = Roster.where(:conditions => ["record_date >= ? and record_date <= ? ", @date_from, @date_to])
+    @serch_result = Roster.where("user_id == ? and record_date >= ? and record_date <= ? ",current_user.id, @date_from, @date_to)
   end
 
   def update
@@ -37,13 +37,13 @@ class KinmuController < ApplicationController
     begin
       update_db = Roster.find_by!(record_date: params[:roster][:record_date], user_id: params[:roster][:user_id])
     rescue Exception => e
-      puts "--データ新規追加 #{e}"
+      puts "--データ新規追加 #{e} #{params[:roster]} #{params[:roster][:end_working].class}"
     end
     begin
       update_db.record_date      = params[:roster][:record_date]
       update_db.user_id          = params[:roster][:user_id]
-      update_db.working_start    = params[:roster][:working_start]
-      update_db.end_working      = params[:roster][:end_working]
+      update_db.working_start    = params[:roster][:record_date] + " " + params[:roster][:working_start]
+      update_db.end_working      = params[:roster][:record_date] + " " + params[:roster][:end_working]
       update_db.work_description = params[:roster][:work_description]
       result = "error"
       if update_db.save
