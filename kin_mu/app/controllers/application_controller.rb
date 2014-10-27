ed6c5bde402c5080322879251d94408cfc8d7802
山崎孝洋 <tak_3_zaki3@yahoo.co.jp>
2014-10-28 00:48:56 +0900

    以下修正 　・リンク切れ 　・５秒後移動 とか・・・・

--- kin_mu/app/controllers/application_controller.rb
+++ kin_mu/app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  # Setting Deafault jump
+  def after_sign_in_path_for(resource)
+    admin_root_path = "/kinmu/show"
+  end
+  def after_sign_out_path_for(resource_or_scope)
+    admin_root_path = "/users/sign_in"
+  end
 end
